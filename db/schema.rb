ff9c1e18935b0eff18aec1d75a52ae4d596c2894
lmunozm1702 <53025062+lmunozm1702@users.noreply.github.com>
2022-06-29 17:01:01 -0400

    Second Commit +comments field

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_06_29_203541) do
+ActiveRecord::Schema.define(version: 2022_06_29_205605) do
 
   create_table "users", force: :cascade do |t|
     t.string "email"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.integer "age"
+    t.text "comment"
   end
 
 end
